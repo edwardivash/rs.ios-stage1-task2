2b56d58976d8b9bfaf652f693daffa8627405309
Eduard Ivash <eduardivash@gmail.com>
2020-03-23 22:10:55 +0300

    add task 3

--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
@@ -4,8 +4,19 @@
 // Complete the abbreviation function below.
 - (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b {
     
-
-    return @"";
+    NSMutableString *mutableA = [NSMutableString stringWithString:[a uppercaseString]];
+    NSMutableString *result = [[NSMutableString alloc]init];
+    
+    for (int i = 0; i < [mutableA length]; i++) {
+        for (int j = 0; j < [b length]; j++) {
+            if ([mutableA characterAtIndex:i] == [b characterAtIndex:j]) {
+                [result appendFormat:@"%c", [mutableA characterAtIndex:i]];
+            }
+        }
+    }
+    
+    return [result isEqualToString:b] ? @"YES":@"NO";
+    
 }
 
 @end
